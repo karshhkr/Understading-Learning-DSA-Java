05950145a190a35c6fb1c83e318009d4be419766
Utkarsh Kumar Dabgarwal <utkarshkumardabgarwal@gmail.com>
2026-05-18 23:14:35 +0530

    files changed

---
+++ src/main/java/com/example/DSA/java/Searching/BinarySearchLeetcode/FindPeakElement1095.java
@@ -0,0 +1,7 @@
+package com.example.DSA.java.Searching.BinarySearchLeetcode;
+//https://leetcode.com/problems/find-in-mountain-array/
+public class FindPeakElement1095 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/DSA/java/Searching/BinarySearchLeetcode/PeakIndexInMountainArray.java
+++ src/main/java/com/example/DSA/java/Searching/BinarySearchLeetcode/PeakIndexInMountainArray.java
@@ -1,4 +1,31 @@
 package com.example.DSA.java.Searching.BinarySearchLeetcode;
-
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
 public class PeakIndexInMountainArray {
+    public static void main(String[] args) {
+int arr[]={1,2,3,5,7,6,3,2};
+
+    } public int peakIndexInMountainArray(int[] arr) {
+
+
+    int start=0;
+    int end=arr.length-1;
+        while(start<end){
+        int mid =start+(end-start)/2;
+        if(arr[mid]>arr[mid+1]){
+            /// /this may be ans but check left side
+            end=mid;//  we at the decreasing side of the array
+
+        }
+        /// /this may be also the ans but check right side
+        else if (arr[mid]<arr[mid+1]){
+            start=mid+1;//  we at the increasing side of the array
+        }
+    }
+        // start= end pointing largest element of the array
+        // we have search the conditions for the largest element now the the elemet would at start and end
+
+        return start;// we can return end too;
+}
+
+
 }
